ba76e74f7a34dd745d24200d9f88ef75d4909664
Lu6fer <lu6fer2604@gmail.com>
2016-02-29 21:10:58 +0100

    add bootstrap

---
+++ resources/views/layout/html.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Emprunt - @yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
+</head>
+<body>
+    <nav>
+        @sidebar
+    </nav>
+    <script type="application/javascript" src="{{ asset('bower_components/jquery/dist/jquery.min.js') }}"></script>
+    <script type="application/javascript" src="{{ asset('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+</body>
+</html>
