e6dae61594632fc045fea0a66afe69578b0282b8
SXRK6690 <fhunkeler.ext@orange.com>
2017-01-12 11:04:30 +0100

    migrate

--- migrate-database.sh
+++ migrate-database.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e 
 echo "############################################# Migrating database 'php artisan migrate'... #################################################"
-php artisan migrate --env="openshift"
+php artisan migrate:refresh --env="openshift"
 echo "####################################################### Application seed ##################################################################"
 php artisan db:seed
